ce169454f33e6169b44fa9b8b714d747628e65b2
p18750 <david.lichtsteiner@css.ch>
2020-07-13 20:45:18 +0200

    Init: Add Beta Teaser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BetaTeaserComponent } from './beta-teaser/beta-teaser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BetaTeaserComponent
   ],
   imports: [
     BrowserModule,
