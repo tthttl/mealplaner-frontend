c4c5478eee877992b909c30b6dfd96101350cb8e
p19210 <attila.toth@css.ch>
2020-09-21 16:58:51 +0200

    test v1

--- src/app/login/components/login-form/login-form.component.spec.ts
+++ src/app/login/components/login-form/login-form.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslatePipe } from '../../../i18n/pipes/translate.pipe';
+import { SharedModule } from '../../../shared/shared.module';
 
 import { LoginFormComponent } from './login-form.component';
 
@@ -10,7 +11,7 @@ describe('LoginFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule],
+      imports: [ReactiveFormsModule, SharedModule],
       declarations: [LoginFormComponent, TranslatePipe],
       providers: [
         {
@@ -29,6 +30,16 @@ describe('LoginFormComponent', () => {
   });
 
   it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  fit('button should be disabled when inputs are empty', () => {
+    const hostElement = fixture.nativeElement;
+    const button = hostElement.querySelector('button');
+    expect(button.disabled).toBeTruthy();
+  });
+
+  it('should emit credentials when inputs are filled and button is clicked', () => {
     expect(component).toBeTruthy();
   });
 });
