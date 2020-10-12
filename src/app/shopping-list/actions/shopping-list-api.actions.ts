7db3298dcaafd7af56e1ce19f8bc21c51d25db19
David Lichtsteiner <david.lichtsteiner@css.ch>
2020-10-12 22:40:28 +0200

    Add Actions

--- src/app/shopping-list/actions/shopping-list-api.actions.ts
+++ src/app/shopping-list/actions/shopping-list-api.actions.ts
@@ -1,9 +1,41 @@
 import { createAction, props } from '@ngrx/store';
-import { ShoppingListItem, User } from '../../shared/model/model';
+import { ShoppingList, ShoppingListItem, ShoppingListItemMovedEvent, User } from '../../shared/model/model';
 
 export const loadShoppingListItemsSuccess = createAction(
   '[Shopping List API] Load Shopping List Items Success',
-  props<{ items: ShoppingListItem[] }>()
+  props<{ shoppingListId: string, shoppingListItems: ShoppingListItem[] }>()
 );
 export const loadShoppingListItemsFailure = createAction('[Shopping List API] Load Shopping List Items Failure');
 
+export const loadShoppingListsSuccess = createAction(
+  '[Shopping List API] Load Shopping Lists Success',
+  props<{ shoppingLists: ShoppingList[] }>()
+);
+export const loadShoppingListsFailure = createAction('[Shopping List API] Load Shopping Lists Failure');
+
+export const addShoppingListItemSuccess = createAction(
+  '[Shopping List API] Add Shopping List Items Success',
+  props<{ shoppingListItem: ShoppingListItem }>()
+);
+export const addShoppingListItemFailure = createAction(
+  '[Shopping List API] Add Shopping List Items Failure',
+  props<{ shoppingListItem: ShoppingListItem }>()
+);
+
+export const deleteShoppingListItemSuccess = createAction(
+  '[Shopping List API] Delete Shopping List Items Success',
+  props<{ shoppingListItem: ShoppingListItem }>()
+);
+export const deleteShoppingListItemFailure = createAction(
+  '[Shopping List API] Delete Shopping List Items Failure',
+  props<{ shoppingListItem: ShoppingListItem }>()
+);
+
+export const updateShoppingListItemSuccess = createAction(
+  '[Shopping List API] Delete Shopping List Items Success',
+  props<ShoppingListItemMovedEvent>()
+);
+export const updateShoppingListItemFailure = createAction(
+  '[Shopping List API] Delete Shopping List Items Failure',
+  props<ShoppingListItemMovedEvent>()
+);
--- src/app/shopping-list/actions/shopping-list-container.actions.ts
+++ src/app/shopping-list/actions/shopping-list-container.actions.ts
@@ -1,5 +1,25 @@
 import { createAction, props } from '@ngrx/store';
-import { User } from '../../shared/model/model';
+import { AddShoppingListItemEvent, ShoppingListItem } from '../../shared/model/model';
+
+export const loadShoppingLists = createAction('[Shopping List Container] Load Shopping Lists');
+
+export const changeShoppingList = createAction( '[Shopping List Container] Change Shopping List', props<{shoppingListId: string}>());
 
 export const loadShoppingListItems = createAction('[Shopping List Container] Load Shopping List Items', props<{ id: string }>());
+
+export const addShoppingListItem = createAction(
+  '[Shopping List Container] Add Shopping List Items',
+  props<{ shoppingListId: string, shoppingListItem: ShoppingListItem }>()
+);
+
+export const deleteShoppingListItem = createAction(
+  '[Shopping List Container] Delete Shopping List Items',
+  props<{ shoppingListId: string, shoppingListItem: ShoppingListItem }>()
+);
+
+export const moveShoppingListItem = createAction(
+  '[Shopping List Container] Move Shopping List Items',
+  props<{ shoppingListId: string, currentIndex: number, previousIndex: number }>()
+);
+
 
