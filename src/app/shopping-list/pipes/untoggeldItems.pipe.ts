a17deb4af98fc98b5bf467176f68a65c51787696
p19210 <attila.toth@css.ch>
2020-10-21 18:44:19 +0200

    fix build

--- src/app/shopping-list/pipes/untoggeldItems.pipe.ts
+++ src/app/shopping-list/pipes/untoggeldItems.pipe.ts
@@ -3,8 +3,10 @@ import { ShoppingListItem } from '../../shared/model/model';
 
 @Pipe({name: 'unToggled'})
 export class UnToggledPipe implements PipeTransform {
-  transform(value: ShoppingListItem[]): ShoppingListItem[] {
-    console.log(value);
-    return value.filter(shoppingListItem => !shoppingListItem.isChecked);
+  transform(value: ShoppingListItem[] | null): ShoppingListItem[] {
+    if (value) {
+      return value.filter(shoppingListItem => !shoppingListItem.isChecked);
+    }
+    return [];
   }
 }
