45fba0e23bb2084414b251cf7d4737fa5c138f9a
David Lichtsteiner <david.lichtsteiner@css.ch>
2020-10-21 16:14:59 +0200

    Fix pipe to deal with inital null

--- src/app/shopping-list/pipes/untoggeldItems.pipe.ts
+++ src/app/shopping-list/pipes/untoggeldItems.pipe.ts
@@ -3,8 +3,10 @@ import { ShoppingListItem } from '../../shared/model/model';
 
 @Pipe({name: 'unToggled'})
 export class UnToggledPipe implements PipeTransform {
-  transform(value: ShoppingListItem[]): ShoppingListItem[] {
-    console.log(value);
+  transform(value: ShoppingListItem[] | null): ShoppingListItem[] {
+    if (!value) {
+      return [];
+    }
     return value.filter(shoppingListItem => !shoppingListItem.isChecked);
   }
 }
