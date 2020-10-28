3de03b6bc562f869643d9cd7f58eb7005d0b097a
David Lichtsteiner <david.lichtsteiner@css.ch>
2020-10-28 20:05:23 +0100

    Test Proxy

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   showBetaTeaser: false,
-  apiUrl: 'https://api.mealplaner.app',
-  authUrl: 'https://api.mealplaner.app',
+  apiUrl: 'https://beta.mealplaner.app/api',
+  authUrl: 'https://beta.mealplaner.app/api',
 };
