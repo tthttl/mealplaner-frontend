59da03f982c9c2cef4a13e487153759275037399
p19210 <attila.toth@css.ch>
2020-12-20 17:11:10 +0100

    style recipe component

--- src/app/features/cookbook/cookbook.module.ts
+++ src/app/features/cookbook/cookbook.module.ts
@@ -2,6 +2,7 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -9,16 +10,16 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { EffectsModule } from '@ngrx/effects';
 import { TranslatePipe } from '../../shared/pipes/translate.pipe';
 import { SharedModule } from '../../shared/shared.module';
-import { CookbookPageComponent } from './pages/cookbook-page/cookbook-page.component';
+import { AddRecipeDialogComponent } from './components/add-recipe-dialog/add-recipe-dialog.component';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 import { CookbookContainerComponent } from './containers/cookbook-container/cookbook-container.component';
 import { RecipeContainerComponent } from './containers/recipe-container/recipe-container.component';
 import { CookbookRoutingModule } from './cookbook-routing.module';
-import { CookbookEffects } from './store/effects/cookbook.effects';
+import { CookbookPageComponent } from './pages/cookbook-page/cookbook-page.component';
+import { RecipePageComponent } from './pages/recipe-page/recipe-page.component';
 import { CookbookService } from './services/cookbook.service';
 import { RecipeService } from './services/recipe.service';
-import { AddRecipeDialogComponent } from './components/add-recipe-dialog/add-recipe-dialog.component';
-import { RecipePageComponent } from './pages/recipe-page/recipe-page.component';
+import { CookbookEffects } from './store/effects/cookbook.effects';
 
 
 @NgModule({
@@ -30,18 +31,19 @@ import { RecipePageComponent } from './pages/recipe-page/recipe-page.component';
     AddRecipeDialogComponent,
     CookbookPageComponent,
   ],
-  imports: [
-    CommonModule,
-    EffectsModule.forFeature([CookbookEffects]),
-    CookbookRoutingModule,
-    SharedModule,
-    ReactiveFormsModule,
-    DragDropModule,
-    FontAwesomeModule,
-    MatSlideToggleModule,
-    MatDialogModule,
-    MatCheckboxModule,
-  ],
+    imports: [
+        CommonModule,
+        EffectsModule.forFeature([CookbookEffects]),
+        CookbookRoutingModule,
+        SharedModule,
+        ReactiveFormsModule,
+        DragDropModule,
+        FontAwesomeModule,
+        MatSlideToggleModule,
+        MatDialogModule,
+        MatCheckboxModule,
+        MatButtonToggleModule,
+    ],
   providers: [
     {
       provide: TranslatePipe,
